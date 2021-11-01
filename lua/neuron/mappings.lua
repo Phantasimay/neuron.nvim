f131ce9b522986b2835c250c3e9569f3d73ab94c
Fabrizio Fortunato <fortunatof@ryanair.com>
2021-11-01 16:07:49 +0000

    fix(telescope): Fixed mappings

--- lua/neuron/mappings.lua
+++ lua/neuron/mappings.lua
@@ -15,22 +15,22 @@ end
 
 function M.set_keymaps()
   api.nvim_buf_set_keymap(0, "n", "<CR>", ":lua require'neuron'.enter_link()<CR>", {noremap = true, silent = true})
-  M.map_buf("<CR>", "<cmd>lua require'neuron'.enter_link()<CR>")
+  M.map("<CR>", "<cmd>lua require'neuron'.enter_link()<CR>")
 
-  M.map_buf("n", "<cmd>lua require'neuron/cmd'.new_edit(require'neuron/config'.neuron_dir)<CR>")
+  M.map("n", "<cmd>lua require'neuron/cmd'.new_edit(require'neuron/config'.neuron_dir)<CR>")
 
-  M.map_buf("z", "<cmd>lua require'neuron/telescope'.find_zettels()<CR>")
-  M.map_buf("Z", "<cmd>lua require'neuron/telescope'.find_zettels {insert = true}<CR>")
+  M.map("z", "<cmd>lua require'neuron/telescope'.find_zettels()<CR>")
+  M.map("Z", "<cmd>lua require'neuron/telescope'.find_zettels {insert = true}<CR>")
 
-  M.map_buf("b", "<cmd>lua require'neuron/telescope'.find_backlinks()<CR>")
-  M.map_buf("B", "<cmd>lua require'neuron/telescope'.find_backlinks {insert = true}<CR>")
+  M.map("b", "<cmd>lua require'neuron/telescope'.find_backlinks()<CR>")
+  M.map("B", "<cmd>lua require'neuron/telescope'.find_backlinks {insert = true}<CR>")
 
-  M.map_buf("t", "<cmd>lua require'neuron/telescope'.find_tags()<CR>")
+  M.map("t", "<cmd>lua require'neuron/telescope'.find_tags()<CR>")
 
-  M.map_buf("s", "<cmd>lua require'neuron'.rib()<CR>")
+  M.map("s", "<cmd>lua require'neuron'.rib()<CR>")
 
-  M.map_buf("]", "<cmd>lua require'neuron'.goto_next_extmark()<CR>")
-  M.map_buf("[", "<cmd>lua require'neuron'.goto_prev_extmark()<CR>")
+  M.map("]", "<cmd>lua require'neuron'.goto_next_extmark()<CR>")
+  M.map("[", "<cmd>lua require'neuron'.goto_prev_extmark()<CR>")
 end
 
 ---setup the keymappings
